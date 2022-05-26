762a933b10ae8e9f1d14aa85ba23b3d784aeede3
gyalxen69 <blak.curry@gmail.com>
2022-05-26 19:05:42 +0200

    finalpush

--- testing/final.php
+++ testing/final.php
@@ -4,26 +4,45 @@
 <head>
   <title>ALL THE STARS</title>
   <link rel="stylesheet" type="text/css" href="style.css">
+  <style>
+    .hey{
+      display: flex;
+      position: absolute;
+      top: 50px;
+      left: 100px;
+      height: 2px;
+      gap: 40vw;
+      width: auto;
+      justify-content: space-around;
+      background-color: black;
+    }
+
+    p{
+      color: white;
+      height: max-content;
+      width: min-content;
+      font-size: 1.2rem;
+      animation: 1.2s all infinite;
+    }
+
+    @keyframes all {
+      0%, 100%{
+        opacity: 0;
+      }
+      50%{
+        opacity: 100%;
+      }
+    }
+  </style>
+  <script type="text/javascript">
+    user = document.
+  </script>
 </head>
 <body>
+  <div class="hey">
+    <p>gello</p>
+    <p>hi</p>
+  </div>
   <?php include("../starfield.php") ?>
-  <?php
-      if(is_array($fetchData)){
-      $sn=1;
-      foreach($fetchData as $data){
-    ?>
-      <tr>
-      <td><?php echo $sn; ?></td>
-      <td><?php echo $data['username']??''; ?></td>
-     </tr>
-     <?php
-      $sn++;}}else{ ?>
-      <tr>
-        <td colspan="8">
-    <?php echo $fetchData; ?>
-  </td>
-    <tr>
-    <?php
-    }?>
 </body>
 </html>
